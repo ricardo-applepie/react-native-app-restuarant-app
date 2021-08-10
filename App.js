68e20f6c2bdf81852240b813536dbe5a0b4e4745
ricardo-applepie <ricardo@applepie.berlin>
2021-08-10 13:21:41 +0200

    added react native icons

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
 import React from "react";
 import { Text, View, StyleSheet, TextInput, ScrollView ,Image} from "react-native";
+import  Icon  from 'react-native-vector-icons/FontAwesome'
+
 
 const App = ()=>{
 
   return (
     <View style={{ height: '100%' }}>
+      <View style={{height:"85%"}}>
+   
       <View style={Style.container} >
               <Image  source={require('./images/helpling.png')} />
-      </View>
+                     
+
+        </View>
+
       <View style={Style.cutomerNotification}>
           <View >
              <Image style={Style.avatar}  source={require('./images/avatar.png')} />
+    
           </View>
           <View>
-              <Text>Fondungallah Kondem</Text>
+              <Text>Fondungallah Kondem mbancho !i</Text>
           </View>
           <View>
              <Image style={Style.avatar} source={require('./images/avatar.png')} />
           </View>
       </View>
-        <View>
+        <View style={Style.row}>
             <View><Text>My Earnings</Text></View>
-            <View><Text>view history </Text></View>
+            <View><Text>view history... </Text></View>
         </View>
         <View>
-             <View>
+            <View style={[Style.row]}>
                 <View>
                   <Text>Current earnings in August  </Text>
                   <Text>Updated: yesterday  </Text>
@@ -33,39 +41,41 @@ const App = ()=>{
                   <Text>0.00$</Text>
                 </View>
              </View>
-             <View>
-                 <View>
+             <View >
+             <View style={Style.row}>
                    <Text>Estimating earnings this month </Text>
                    <Text> 41.70$ </Text>
-                 </View>
-                <View>
+              </View>
+              <View style={Style.row}>
                   <Text>Estimating earnings this month </Text>
                   <Text> 41.70$ </Text>
                 </View>
             </View>
         </View>
-        <View>
+      </View>
+      <View style={[Style.row, Style.homepageFooter]}>
            <View>
-            <Image style={Style.avatar} source={require('./images/avatar.png')} />
-            <Text>Home</Text>
+          <Icon name="home" size={50} color="900" />       
+             <Text style={{ textAlign: "center" }}>Home</Text>
            </View>
           <View>
-            <Image style={Style.avatar} source={require('./images/avatar.png')} />
-            <Text>Offers</Text>
+          <Icon name="star" size={50} color="900" />
+          <Text style={{ textAlign: "center" }}>Offers!</Text>
           </View>
           <View>
             <Image style={Style.avatar} source={require('./images/avatar.png')} />
-            <Text>Chat</Text>
+          <Text style={{ textAlign: "center"}}>Chat</Text>
           </View>
           <View>
             <Image style={Style.avatar} source={require('./images/avatar.png')} />
-            <Text>Calendar</Text>
+          <Text style={{ textAlign: "center" }}>Calendar!</Text>
           </View>
           <View>
             <Image style={Style.avatar} source={require('./images/avatar.png')} />
-            <Text>More</Text>
+          <Text style={{ textAlign: "center" }}>More!</Text>
           </View>
         </View>
+      
     
     </View>
   )
@@ -87,14 +97,32 @@ const Style = StyleSheet.create(
     },
     cutomerNotification:{
       flexDirection:"row",
-      justifyContent: "space-between"
+      justifyContent: "space-between",
+    
+      marginLeft: 20,
+      marginRight: 20,
+      marginBottom: 20,
+      marginTop:30
      
     }
     ,
     avatar:{
       width: 60,
       height: 65    
+    },
+    row:{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginLeft:20,
+      marginRight:20,
+      marginBottom:20
+    },
+    homepageFooter:{
+      marginLeft:10,
+      marginRight:10,
+      height: "10%",
+     
+
     }
- 
   }
 )
